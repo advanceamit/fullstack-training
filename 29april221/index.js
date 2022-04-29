c599ae991ca2327841b15878081fb3bfe200f8da
advanceamit <amit.kumar@appinessworld.com>
2022-04-29 12:37:33 +0530

    updated

--- 29april221/index.js
+++ 29april221/index.js
@@ -1,3 +1,6 @@
 console.log("hi this is the first code of javascript");
 let sum = 2 + 4;
 console.log(sum);
+alert("javascript running on console")
+var a = prompt("enter number");
+console.log(a);
